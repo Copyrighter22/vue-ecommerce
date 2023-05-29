650e83cdb6470cea13bc1ad12691bd0db16050f5
«Andreu <andreump5@gmail.com>
2023-05-29 21:01:41 +0200

    finalització del video 4

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Pokeballs',
-            image: './assets/images/pokeball.png'
+            image: './assets/images/pokeball.png',
+            inventory: 100,
+            onSale: true
         }
     }
 })
