7d3540646b3fce510208383a24888aba0f541c72
«Andreu <andreump5@gmail.com>
2023-05-30 15:42:03 +0200

    finalització del video 8

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Pokeballs',
-            image: './assets/images/pokeball.png',
-            inventory: 100,
-            inStock: false,
+            description: 'Gotta catch em all!',
             details: ['50% cotton', '30% wool', '20% polyester'],
-            sizes: ['S', 'M', 'L', 'XL'],
+            selectedVariant: 0,
             variants: [
-              { id: 2234, color: 'red' , image: './assets/images/pokeball.png'},
-              { id: 2235, color: 'purple' , image: './assets/images/masterball.webp'},
+              { id: 2234, color: 'red' , image: './assets/images/pokeball.png', quantity: 50},
+              { id: 2235, color: 'purple' , image: './assets/images/masterball.webp', quantity: 0},
             ],
             cart: 0,
 
@@ -20,8 +18,26 @@ const app = Vue.createApp({
             this.cart += 1
         },
 
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            return this.product + ' ' + this.description
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.inStock > 0) {
+                return this.product + ' ' + this.description + ' en venta!'
+            }
+            return ''
+        }
+    
     }
 })
