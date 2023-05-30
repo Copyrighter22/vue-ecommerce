be645f22c50d1a9ca9338661412b2f1b5f52c119
«Andreu <andreump5@gmail.com>
2023-05-30 12:23:49 +0200

    finalització del video 7

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const app = Vue.createApp({
             product: 'Pokeballs',
             image: './assets/images/pokeball.png',
             inventory: 100,
-            onSale: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 2234, color: 'green' , image: './assets/images/pokeball.png'},
-              { id: 2235, color: 'blue' , image: './assets/images/masterball.webp'},
+              { id: 2234, color: 'red' , image: './assets/images/pokeball.png'},
+              { id: 2235, color: 'purple' , image: './assets/images/masterball.webp'},
             ],
             cart: 0,
 
@@ -18,10 +18,6 @@ const app = Vue.createApp({
     methods: {
         addToCart() {
             this.cart += 1
-        },
-
-        removeToCart() {
-            this.cart -= 1
         },
 
         updateImage(variantImage) {
